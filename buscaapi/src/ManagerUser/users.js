57d136367adf879839ab7cb8539ab15ba82dd098
Felipe <machadofelipe2016@outlook.com>
2022-06-06 18:40:41 -0300

    c16-adding some testing

---
+++ buscaapi/src/ManagerUser/users.js
@@ -0,0 +1,13 @@
+import React,{useContext} from 'react'
+import AllManage from '../ManageContext/Context'
+
+
+const User = (props) => {
+    const {allusers, updatingpeople} = useContext(AllManage)
+
+
+    return (<div>
+        
+    </div>)
+
+}
