25576c346d5bbe7f1d87bf43b5d85403ae31b7c8
Felipe <machadofelipe2016@outlook.com>
2022-06-06 14:21:28 -0300

    c3-adding navbar in page principles to make testing here

--- buscaapi/src/App.js
+++ buscaapi/src/App.js
@@ -1,10 +1,10 @@
-
 import './App.css';
+import NavbarUser from './ManagerUser/navbaruser';
 
 function App() {
   return (
     <div className="App">
-      
+    <NavbarUser/>      
     </div>
   );
 }
