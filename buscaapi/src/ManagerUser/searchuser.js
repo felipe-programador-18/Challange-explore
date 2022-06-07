d8eabbe6283392691bb0bf352e922c975bd5fe20
Felipe <machadofelipe2016@outlook.com>
2022-06-07 20:18:05 -0300

    c19- try some code

--- buscaapi/src/ManagerUser/searchuser.js
+++ buscaapi/src/ManagerUser/searchuser.js
@@ -1,14 +1,24 @@
 import React,{useState} from 'react'
 
 
+
 const SeachBar = (props) => {
-  const [search, setsearch] =  useState("Mr")
-  const {Onsearch} = props
-   
+
+  const [search, setsearch] =  useState("")
+  const {Onsearch} = props;
+  const anotertest = Object.keys(search).map((one) => one)
+  //const verify = anotertest.filter()
+  //console.log('testing here',verify)
+  //console.log('test', search)
+  //const teste = search.toLowerCase()
+  //const treat = teste.filter((call) => call.toLowerCase().includes(teste))  
+  
+
   const GetSearch = (evt) => {
       setsearch(evt.target.value)
       if(evt.target.value === 0){
           Onsearch(undefined)
+         
       }
   }  
 
@@ -20,6 +30,12 @@ const SeachBar = (props) => {
 
     return( <div className='text-center'>
         <input  className='text-center bg-dark m-2' type='text' placeholder='Search' onChange={GetSearch} />
+
+        {/*<ul>
+            {treat.map((call) => (
+                <li key={call} >{call}</li>
+            ))}
+            </ul>*/}
         <button className='d-block m-auto ' onClick={ClickSearch}>Search</button>
     </div>
      )
