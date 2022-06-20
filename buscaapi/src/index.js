4b4fba6d6a6c9603d541a27cb147ebe4016815b8
Felipe <machadofelipe2016@outlook.com>
2022-06-20 18:59:39 -0300

    c31- adding all dependence to get used react modal

--- buscaapi/src/index.js
+++ buscaapi/src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import "bootstrap/dist/css/bootstrap.css"
+import "@popperjs/core"
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Modal from 'react-modal'
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/react-modal/3.14.3/react-modal.min.js"
 integrity="sha512-MY2jfK3DBnVzdS2V8MXo5lRtr0mNRroUI9hoLVv2/yL3vrJTam3VzASuKQ96fLEpyYIT4a8o7YgtUs5lPjiLVQ=="
@@ -11,6 +13,7 @@ crossorigin="anonymous"
 referrerpolicy="no-referrer"></script>
 
 
+Modal.setAppElement('#root');
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
