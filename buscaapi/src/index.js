1f5174849a3f604671c8c25691d5cec8c0d5e214
Felipe <machadofelipe2016@outlook.com>
2022-06-20 18:12:35 -0300

    c24-adding dependence inside project about reactmodal

--- buscaapi/src/index.js
+++ buscaapi/src/index.js
@@ -5,6 +5,12 @@ import "bootstrap/dist/css/bootstrap.css"
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/react-modal/3.14.3/react-modal.min.js"
+integrity="sha512-MY2jfK3DBnVzdS2V8MXo5lRtr0mNRroUI9hoLVv2/yL3vrJTam3VzASuKQ96fLEpyYIT4a8o7YgtUs5lPjiLVQ=="
+crossorigin="anonymous"
+referrerpolicy="no-referrer"></script>
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
